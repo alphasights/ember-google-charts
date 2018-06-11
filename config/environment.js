d2b1b74da5db3f39853362dbd648028272a9d950
saurav jain <jain.saurav@hotmail.com>
2018-06-11 11:13:19 -0400

    Remove env variable declaration in config file

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,6 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function(environment) {
-  var ENV = {
-    mapsApiKey: process.env.GOOGLE_MAPS_API_KEY
-  }
-
-  return ENV;
+module.exports = function(/* environment, appConfig */) {
+  return {};
 };
